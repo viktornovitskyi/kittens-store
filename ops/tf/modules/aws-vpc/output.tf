49d18e4752621f2eb020af8cdbdc51dd86271b35
Viktor Novitskyi <viktor.n@matic.com>
2021-09-03 07:45:14 +0300

    Fix vpc output.tf

--- ops/tf/modules/aws-vpc/output.tf
+++ ops/tf/modules/aws-vpc/output.tf
@@ -2,5 +2,8 @@ output "vpc_id" {
   value = aws_vpc.global-vpc.id
 }
 output "route_table" {
-  value = aws_route_table.pubic-route-table.id
+  value = aws_route_table.global-route-table.id
+}
+output "subnets" {
+  value = aws_subnet.global-subnets
 }
