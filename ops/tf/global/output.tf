c2b5504afcd888eaadfe35cdb503092b7f03f615
Viktor Novitskyi <46745305+viktornovitskyi@users.noreply.github.com>
2021-09-12 11:09:49 +0300

    infra (#4)
    * Add infrastructure.yml

--- ops/tf/global/output.tf
+++ ops/tf/global/output.tf
@@ -1,6 +1,6 @@
-locals {
-  db = module.main-database
-}
+//locals {
+//  db = module.main-database
+//}
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
@@ -12,24 +12,24 @@ output "subnet_ids" {
 output "availability_zones" {
   value = [for subnet in module.vpc.subnets : "${subnet.id} => ${subnet.availability_zone}"]
 }
-
-output "database_host" {
-  value = local.db.endpoint
-}
-
-output "rds-sg-id" {
-  value = local.db.db-connection-sg-id
-}
-
-output "database_connection_url" {
-  value     = "postgres://${local.db.username}:${local.db.password}@${local.db.endpoint}/${local.db.database_name}"
-  sensitive = true
-}
-
-output "puts_database_connection_url" {
-  value = "terraform output database_connection_url"
-}
-
-output "eks-cluster-endpoint" {
-  value = module.web-eks-cluster.endpoint
-}
+//
+//output "database_host" {
+//  value = local.db.endpoint
+//}
+//
+//output "rds-sg-id" {
+//  value = local.db.db-connection-sg-id
+//}
+//
+//output "database_connection_url" {
+//  value     = "postgres://${local.db.username}:${local.db.password}@${local.db.endpoint}/${local.db.database_name}"
+//  sensitive = true
+//}
+//
+//output "puts_database_connection_url" {
+//  value = "terraform output database_connection_url"
+//}
+//
+//output "eks-cluster-endpoint" {
+//  value = module.web-eks-cluster.endpoint
+//}
