dd72b96101958b74424cc582dd884b2106511154
Viktor Novitskyi <viktor.n@matic.com>
2021-09-12 13:30:35 +0300

    fix

--- ops/tf/global/output.tf
+++ ops/tf/global/output.tf
@@ -12,24 +12,20 @@ output "subnet_ids" {
 output "availability_zones" {
   value = [for subnet in module.vpc.subnets : "${subnet.id} => ${subnet.availability_zone}"]
 }
-//
-//output "database_host" {
-//  value = local.db.endpoint
-//}
-//
+
+output "database_host" {
+  value = local.db.endpoint
+}
+
 //output "rds-sg-id" {
 //  value = local.db.db-connection-sg-id
 //}
 //
-//output "database_connection_url" {
-//  value     = "postgres://${local.db.username}:${local.db.password}@${local.db.endpoint}/${local.db.database_name}"
-//  sensitive = true
-//}
-//
-//output "puts_database_connection_url" {
-//  value = "terraform output database_connection_url"
-//}
-//
+output "database_connection_url" {
+  value     = "postgres://${local.db.username}:${local.db.password}@${local.db.endpoint}/${local.db.database_name}"
+  sensitive = true
+}
+
 //output "eks-cluster-endpoint" {
 //  value = module.web-eks-cluster.endpoint
 //}
